6dad8c9ead6c724edf0329aa90d663895eb8ab3d
Mendelt Siebenga <mendelt@hotmail.com>
2010-10-09 10:07:28 +0200

    Added test for adding dictionary items

---
+++ src/WeakDictionary.Facts/WeakDictionaryContext/WhenAddingItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace WeakDictionary.WeakDictionaryContext
+{
+    public class WhenAddingItem
+    {
+        public WeakDictionary<object, object> dictionary = new WeakDictionary<object, object>( );
+
+        object key = new object(  );
+        object item = new object(  );
+
+        public WhenAddingItem()
+        {
+            dictionary.Add(key, item);
+        }
+
+        [Fact]
+        public void ItemShouldBeInDictionary()
+        {
+            Assert.Equal(item, dictionary[key]);
+        }
+    }
+}
---
+++ src/WeakDictionary.Facts/WeakReferenceContext/WeakReferenceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace WeakDictionary.WeakReferenceContext
+{
+    public class WeakReferenceTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var obj = new object( );
+            var reference = new WeakReference(obj);
+            Assert.True( reference.IsAlive );
+            obj = null;
+            GC.Collect();
+            Assert.False( reference.IsAlive );
+        }
+    }
+}
